e3e5ec995527690554da4bb59ae888ae2ba12a27
Yarenhi <yarenhi@gmail.com>
2024-01-22 19:02:42 -0600

    Subiendo proyecto de glitch

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
-/*
-  Aquí va tu JavaScript
-*/
+function Correcta1()  {alert("Las Arañas tienen 8 patas")}
+function Correcta2(){alert("El Pulpo es un animal invertebrado")}
+function Correcta3(){alert("El Ajolote es endémico de México")}
+ console.log(hola)
